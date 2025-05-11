9743b03139d5ef108c2d28dd5f56b31faa7dbf93
Muhammad Hashaam Khan <52697902+invinciblepy@users.noreply.github.com>
2025-05-12 00:28:53 +0500

    Update main.py
    update description of parser

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from wlw.scraper import WLWScraper
 
 def main():
-    parser = argparse.ArgumentParser(description="Gelbe Seiten Scraper CLI")
+    parser = argparse.ArgumentParser(description="WLW Scraper CLI")
     parser.add_argument('-u', '--url', required=True, help='URL of the website to scrape')
     args = parser.parse_args()
     scraper = WLWScraper(url=args.url)
